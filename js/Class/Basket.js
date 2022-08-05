5e92e0df122a7ce783e6669358484862eaca43e7
Vladyslav Zubov <deejay95@ukr.net>
2022-08-05 15:17:24 +0300

    f

--- js/Class/Basket.js
+++ js/Class/Basket.js
@@ -10,20 +10,18 @@ class Basket {
    };
 
    updateBasketAndCalculate() {
-      const doc = document.querySelector('.block-basket');
+      const block = document.querySelector('.block-basket');
       let stringBasket = `Баланс ${balance} грн. <br>`;
       this.sumOrderTotal = this.basketArr.reduce((total, obj) => total + obj.price, 0)
       stringBasket += this.basketArr.reduce((total, obj) => total += `${obj.names} ${obj.price} грн <br>`, '')
 
       stringBasket += `Итого:${this.sumOrderTotal} грн <button class="buy">Оформить заказ</button>`;
-      doc.innerHTML = stringBasket;
-      console.log(this.sumOrderTotal);
+      block.innerHTML = stringBasket;
       document.querySelector('.buy').onclick = this.aproveOrder.bind(this);
    };
 
    aproveOrder() {
       const block = document.querySelector('.block-basket');
-      console.log(this.sumOrderTotal);
       if (this.sumOrderTotal <= balance) {
          balance = balance - this.sumOrderTotal;
          this.basketArr = []
